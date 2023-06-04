28417632a29145f1f2cc5298f292137389693437
Arshad Javeed <37022146+arshad171@users.noreply.github.com>
2023-06-04 20:19:19 +0200

    stick URDF

--- scripts/calc_intertia.py
+++ scripts/calc_intertia.py
@@ -1,13 +1,21 @@
 # mass of cf = 0.027 kgs
 
-### cylinder
-HEIGHT = 0.3
-RADIUS = 0.005
+# ### cylinder
+# HEIGHT = 0.3
+# RADIUS = 0.005
+# MASS = 0.027 * 0.1
+
+# ixx = 1 / 12 * MASS * (3 * RADIUS**2 + HEIGHT**2)
+# iyy = 1 / 12 * MASS * (3 * RADIUS**2 + HEIGHT**2)
+# izz = 1 / 2 * MASS * RADIUS**2
+
+### shere
+RADIUS = 0.02
 MASS = 0.027 * 0.1
 
-ixx = 1 / 12 * MASS * (3 * RADIUS**2 + HEIGHT**2)
-iyy = 1 / 12 * MASS * (3 * RADIUS**2 + HEIGHT**2)
-izz = 1 / 2 * MASS * RADIUS**2
+ixx = 2 / 3 * MASS * RADIUS**2
+iyy = 2 / 3 * MASS * RADIUS**2
+izz = 2 / 3 * MASS * RADIUS**2
 
 print(f"{ixx=}")
 print(f"{iyy=}")
